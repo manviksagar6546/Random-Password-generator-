786caf0cfb2aa25bd4692a7f026b66b3b9fe2480
manviksagar6546 <75376796+manviksagar6546@users.noreply.github.com>
2024-04-21 23:24:16 +0530

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Tester{
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int length = sc.nextInt();
+      System.out.println(generatePassword(length));
+   }
+
+   private static char[] generatePassword(int length) {
+      String capitalCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      String lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+      String specialCharacters = "!@#$";
+      String numbers = "1234567890";
+      String combinedChars = capitalCaseLetters + lowerCaseLetters + specialCharacters + numbers;
+      Random random = new Random();
+      char[] password = new char[length];
+
+      password[0] = lowerCaseLetters.charAt(random.nextInt(lowerCaseLetters.length()));
+      password[1] = capitalCaseLetters.charAt(random.nextInt(capitalCaseLetters.length()));
+      password[2] = specialCharacters.charAt(random.nextInt(specialCharacters.length()));
+      password[3] = numbers.charAt(random.nextInt(numbers.length()));
+   
+      for(int i = 4; i< length ; i++) {
+         password[i] = combinedChars.charAt(random.nextInt(combinedChars.length()));
+      }
+      return password;
+   }
+}
